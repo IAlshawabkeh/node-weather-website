5bf43eba5d6da9c3eb3fbd97b0cea29878629b5f
ialshawabkeh <v-ialshawabkeh@expedia.com>
2020-05-27 04:49:26 +0300

    Init commit

---
+++ src/app.js
@@ -0,0 +1,103 @@
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+const geoCode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
+
+
+const app = express();
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+const name = 'Ihab Alshawabkeh';
+
+// Setup handlebars engine and view location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About page',
+        name
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpMessage: 'Here is the help text',
+        title: 'Help',
+        name
+    })
+});
+
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address!!!!'
+        })
+    };
+
+    geoCode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({ error });
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error });
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        });
+    });
+});
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+
+    console.log(req.query);
+    res.send({
+        products: []
+    });
+});
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+       title: 'Erro Page',
+       name,
+       errorMessage: 'Help article not found!'
+    });
+});
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: 'Error Page',
+        name,
+        errorMessage: 'Page not found!'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
---
+++ src/utils/forecast.js
@@ -0,0 +1,19 @@
+const postmanRequest = require('postman-request');
+
+const forecast = (latitude, longitude, callback) => {
+    const weatherUrl = `http://api.weatherstack.com/current?access_key=63e08a16347e4c66141d7e8df6e916ee&query=${encodeURIComponent(latitude)},${encodeURIComponent(longitude)}`;
+    postmanRequest(weatherUrl, {json: true}, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined); 
+        } else if (body.error) {
+            callback(body.error.info, undefined);
+        } else {
+            const weatherDescription = body.current.weather_descriptions[0];
+            const temperature = body.current.temperature;
+            const feelsLike = body.current.feelslike;
+            callback(undefined, `${weatherDescription}. It is crrently ${temperature} degrees out. It feels like ${feelsLike} degrees out.`);
+        }
+    });
+}
+
+module.exports = forecast
